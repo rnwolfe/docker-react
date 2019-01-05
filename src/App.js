b974e65208ece7636a6189007dd76c1018383696
Ryan Wolfe <rn.wolfe@gmail.com>
2019-01-05 16:15:52 -0500

    change app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to the app, Ryan!
+            Welcome to the app, random internet person!
           </p>
           <a
             className="App-link"
